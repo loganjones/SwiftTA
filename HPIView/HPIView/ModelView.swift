7515a00c9f477a17ba3af774defa2a15d26b5b3d
Logan Jones <logan.b.jones@gmail.com>
2016-11-27 22:39:02 -0800

    some model view updates

--- HPIView/HPIView/ModelView.swift
+++ HPIView/HPIView/ModelView.swift
@@ -13,17 +13,38 @@ import OpenGL
 class Model3DOView: NSOpenGLView {
     
     var displayList: GLuint?
+    var wireframe: GLuint?
     
     override func draw(_ dirtyRect: NSRect) {
         reshape(width: Int(bounds.size.width), height: Int(bounds.size.height))
         initScene()
         
-        glClearColor(0.95, 0.95, 0.95, 0)
+        glClearColor(0.95, 0.95, 0.95, 1)
         glClear(GLbitfield(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT))
         glPushMatrix()
-        glRotatef(75, 1.0, 0.0, 0.0)
-        glRotatef(30, 0.0, 1.0, 0.0)
+        
+        let perspective: [GLfloat] = [
+            -1,   0,   0,   0,
+             0,   1,   0,   0,
+             0,-0.5,   1,   0,
+             0,   0,   0,   1
+        ]
+        glMultMatrixf(perspective)
+        glRotatef(-90, 0.0, 0.0, 1.0)
+//        glRotatef(65, 1.0, 0.0, 0.0)
+//        glRotatef(30, 0.0, 1.0, 0.0)
+        
+        glPolygonMode(GLenum(GL_FRONT), GLenum(GL_FILL))
+        glEnable(GLenum(GL_LIGHTING))
+        glDepthFunc(GLenum(GL_LESS))
         if let model = displayList { glCallList(model) }
+        
+        //glPolygonMode(GLenum(GL_FRONT), GLenum(GL_LINE))
+        glDisable(GLenum(GL_LIGHTING))
+        glDepthFunc(GLenum(GL_LEQUAL))
+        glColor3dv([0.3, 0.3, 0.3, 1])
+        if let model = wireframe { glCallList(model) }
+        
         glPopMatrix()
         glFlush()
     }
@@ -32,14 +53,18 @@ class Model3DOView: NSOpenGLView {
         let data = try Data(contentsOf: modelURL)
         openGLContext?.makeCurrentContext()
         
-        let model = glGenLists(1)
-        glNewList(model, GLenum(GL_COMPILE))
+        let model = glGenLists(2)
         data.withUnsafeBytes { (memory: UnsafePointer<UInt8>) -> Void in
-            ModelGL.drawPiece(atOffset: 0, in: memory)
+            glNewList(model, GLenum(GL_COMPILE))
+            ModelGL.drawFillModel(from: memory)
+            glEndList()
+            glNewList(model + 1, GLenum(GL_COMPILE))
+            ModelGL.drawWireModel(from: memory)
+            glEndList()
         }
-        glEndList()
         
         displayList = model
+        wireframe = model + 1
         setNeedsDisplay(bounds)
     }
     
@@ -56,14 +81,19 @@ class Model3DOView: NSOpenGLView {
     }
     
     func reshape(width: Int, height: Int) {
-        let h = GLdouble(height) / GLdouble(width)
         glViewport(0, 0, GLsizei(width), GLsizei(height))
+        
         glMatrixMode(GLenum(GL_PROJECTION))
         glLoadIdentity()
-        glFrustum(-1.0, 1.0, -h, h, 5.0, 360.0)
+        
+        let aspectRatio = GLdouble(height) / GLdouble(width)
+        let w: GLdouble = 160
+        let scene = (width: w, height: w * aspectRatio)
+        glOrtho(0, scene.width, scene.height, 0, -1024, 256)
+        
         glMatrixMode(GLenum(GL_MODELVIEW))
         glLoadIdentity()
-        glTranslatef(0.0, 0.0, -333.0)
+        glTranslated(scene.width / 2, scene.height / 2, 0.0)
     }
     
 }
@@ -71,7 +101,56 @@ class Model3DOView: NSOpenGLView {
 
 enum ModelGL {
     
-    static func drawPiece(atOffset offset: Int, in memory: UnsafePointer<UInt8>, level: Int = 0) {
+    static func drawWireModel(from memory: UnsafePointer<UInt8>) {
+        drawPiece(atOffset:0, in: memory, level: 0) { (vertices) in
+            glBegin(GLenum(GL_LINE_LOOP))
+            vertices.forEach { glVertex($0) }
+            glEnd()
+        }
+    }
+    
+    static func drawFillModel(from memory: UnsafePointer<UInt8>) {
+        drawPiece(atOffset:0, in: memory, level: 0) { (vertices) in
+            
+            let glModelTriangle = { (a: Int, b: Int, c: Int) in
+                glVertex(vertices[a])
+                glVertex(vertices[b])
+                glVertex(vertices[c])
+            }
+            let glModelNormal = { (a: Int, b: Int, c: Int) in
+                let v1 = vertices[a]
+                let v2 = vertices[b]
+                let v3 = vertices[c]
+                let u = v2 - v1
+                let v = v3 - v1
+                glNormal(u × v)
+            }
+            
+            switch vertices.count {
+            case 3: // Single Triangle
+                glBegin(GLenum(GL_TRIANGLES))
+                glModelNormal(2,1,0)
+                glModelTriangle(2,1,0)
+                glEnd()
+            case 4: // Single Quad, split into two triangles
+                glBegin(GLenum(GL_TRIANGLES))
+                glModelNormal(3,1,0)
+                glModelTriangle(3,1,0)
+                glModelTriangle(3,2,1)
+                glEnd()
+            case 5: // Pentagon, split into three triangles
+                glBegin(GLenum(GL_TRIANGLES))
+                glModelNormal(2,1,0)
+                glModelTriangle(2,1,0)
+                glModelTriangle(3,2,0)
+                glModelTriangle(4,3,0)
+                glEnd()
+            default: ()
+            }
+        }
+    }
+    
+    static func drawPiece(atOffset offset: Int, in memory: UnsafePointer<UInt8>, level: Int, draw: ([Vertex3]) -> Void) {
         
         let object = UnsafeRawPointer(memory + offset).bindMemory(to: TA_3DO_OBJECT.self, capacity: 1).pointee
         //let name = String(cString: memory + object.offsetToObjectName)
@@ -82,51 +161,20 @@ enum ModelGL {
         glTranslate(object.offsetFromParent)
         
         let primitives = UnsafeRawPointer(memory + object.offsetToPrimitiveArray).bindMemoryBuffer(to: TA_3DO_PRIMITIVE.self, capacity: Int(object.numberOfPrimitives))
-        primitives.forEach { (primitive) in
+        for (index, primitive) in primitives.enumerated() {
+            guard index != Int(object.groundPlateIndex) else { continue }
             let indices = UnsafeRawPointer(memory + primitive.offsetToVertexIndexArray).bindMemoryBuffer(to: UInt16.self, capacity: Int(primitive.numberOfVertexIndexes))
-            switch indices.count {
-            case 3: // Single Triangle
-                glBegin(GLenum(GL_TRIANGLES))
-                glNormal((vertices[indices[1]] - vertices[indices[0]]) × (vertices[indices[2]] - vertices[indices[0]]))
-                glVertex(vertices[indices[0]])
-                glVertex(vertices[indices[1]])
-                glVertex(vertices[indices[2]])
-                glEnd()
-            case 4: // Single Quad, split into two triangles
-                glBegin(GLenum(GL_TRIANGLES))
-                glNormal((vertices[indices[1]] - vertices[indices[0]]) × (vertices[indices[3]] - vertices[indices[0]]))
-                glVertex(vertices[indices[0]])
-                glVertex(vertices[indices[1]])
-                glVertex(vertices[indices[3]])
-                glVertex(vertices[indices[1]])
-                glVertex(vertices[indices[2]])
-                glVertex(vertices[indices[3]])
-                glEnd()
-            case 5: // Pentagon, split into three triangles
-                glBegin(GLenum(GL_TRIANGLES))
-                glNormal((vertices[indices[1]] - vertices[indices[0]]) × (vertices[indices[2]] - vertices[indices[0]]))
-                glVertex(vertices[indices[0]])
-                glVertex(vertices[indices[1]])
-                glVertex(vertices[indices[2]])
-                glVertex(vertices[indices[0]])
-                glVertex(vertices[indices[2]])
-                glVertex(vertices[indices[3]])
-                glVertex(vertices[indices[0]])
-                glVertex(vertices[indices[3]])
-                glVertex(vertices[indices[4]])
-                glEnd()
-            default: ()
-            }
+            draw( indices.map({ vertices[$0] }) )
         }
         
         if object.offsetToChildObject != 0 {
-            drawPiece(atOffset: Int(object.offsetToChildObject), in: memory, level: level+1)
+            drawPiece(atOffset: Int(object.offsetToChildObject), in: memory, level: level+1, draw: draw)
         }
         
         glPopMatrix()
         
         if object.offsetToSiblingObject != 0 {
-            drawPiece(atOffset: Int(object.offsetToSiblingObject), in: memory, level: level)
+            drawPiece(atOffset: Int(object.offsetToSiblingObject), in: memory, level: level, draw: draw)
         }
         
     }
@@ -168,8 +216,8 @@ struct Vector3 {
 extension Vertex3 {
     init(_ v: TA_3DO_VERTEX) {
         x = Double(v.x) / LINEAR_CONSTANT
-        y = Double(v.y) / LINEAR_CONSTANT
-        z = Double(v.z) / LINEAR_CONSTANT
+        y = Double(v.z) / LINEAR_CONSTANT
+        z = Double(v.y) / LINEAR_CONSTANT
     }
 }
 
@@ -188,8 +236,8 @@ extension TA_3DO_OBJECT {
     var offsetFromParent: Vector3 {
         return Vector3(
             x: Double(xFromParent) / LINEAR_CONSTANT,
-            y: Double(yFromParent) / LINEAR_CONSTANT,
-            z: Double(zFromParent) / LINEAR_CONSTANT
+            y: Double(zFromParent) / LINEAR_CONSTANT,
+            z: Double(yFromParent) / LINEAR_CONSTANT
         )
     }
 }
