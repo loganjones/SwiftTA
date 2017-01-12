ad1880dc63186c9002dd0031c0bd40cb8b931c96
Logan Jones <logan.b.jones@gmail.com>
2017-01-11 18:37:15 -0800

    We can only view HPIs; so make the Document read-only.

--- HPIView/HPIView/Document.swift
+++ HPIView/HPIView/Document.swift
@@ -15,29 +15,13 @@ import CoreGraphics
 class Document: NSDocument {
     
     var root: HPIItem.Directory?
-
-    override init() {
-        super.init()
-        // Add your subclass-specific initialization here.
-    }
-
-    override class func autosavesInPlace() -> Bool {
-        return true
-    }
     
     override func makeWindowControllers() {
         let controller = HPIBrowserWindowController(windowNibName: "Document")
         addWindowController(controller)
     }
-
-    override func data(ofType typeName: String) throws -> Data {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-    }
     
     override func read(from url: URL, ofType typeName: String) throws {
-        
         do {
             let loaded = try HPIItem(withContentsOf: url).sorted()
             switch loaded {
@@ -49,18 +33,6 @@ class Document: NSDocument {
             Swift.print("ERROR: \(error)")
             throw NSError(domain: NSOSStatusErrorDomain, code: readErr, userInfo: nil)
         }
-        
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        //throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-    }
-
-    override func read(from data: Data, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
 
     enum LoadError: Error {
